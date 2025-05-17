ba9ac6f737e03c7cb4a58a922109dc0326fcf9b9
Zobayer Abdullah <zobayer.abdullah9999@gmail.com>
2025-05-17 11:42:52 +0600

    api

--- routes/api.php
+++ routes/api.php
@@ -23,8 +23,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('tasks', TaskController::class);
     Route::post('/tasks/{task}/assign', [TaskController::class, 'assign']);
 });
-
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
